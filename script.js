c180dc8b730c056bdb3a9512810dae459b9da182
Fitodev <123593063+Fitodev@users.noreply.github.com>
2023-04-25 22:24:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const subjectInput =document.querySelector("#subject")
